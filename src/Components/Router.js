c10d36e1dded8c8e3f4db6a3678aa21ebd30bbc7
onlytaekyu <onlytaekyu@gmail.com>
2020-11-26 22:37:29 +0900

    add

---
+++ src/Components/Router.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
+import Home from "../Routes/Home";
+import TV from "../Routes/TV";
+import Search from "../Routes/Search";
+import Header from "./Header"
+import Detail from "../Routes/Detail";
+
+
+export default () => (
+    <Router>
+        <>
+            <Header />
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/tv"  exact component={TV}/>
+                <Route path="/search"  component={Search}/>
+                <Route path="/move/:id"  component={Detail}/>
+                <Route path="/show/:id"  component={Detail}/>
+                <Redirect from="*" to="/" />
+            </Switch>
+        </>   
+    </Router>
+)
